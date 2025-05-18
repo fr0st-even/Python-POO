c9b02da677599a40fd6b38b71fcaa58fd6a48201
fr0st-even <iosef.joelcv@gmail.com>
2025-05-17 20:19:09 -0600

    Delete lugar.py

--- lugar.py
+++
@@ -1,8 +0,0 @@
-class Lugar:
-    def __init__(self, nombre: str, direccion: str, telefono: str):
-        self.nombre = nombre
-        self.direccion = direccion
-        self.telefono = telefono
-
-    def __str__(self):
-        return f"{self.nombre} - {self.direccion}"
